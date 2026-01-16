ca8fad049cac43c0e263a78516590c128fe998bb
silambu c <144410442+c-silambu@users.noreply.github.com>
2026-01-16 13:00:09 +0530

    Update axios.js

--- billing-frontend/src/services/axios.js
+++ billing-frontend/src/services/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const API=axios.create({
-    baseURL:"http://localhost:3000",
+    baseURL:"https://copy-5mx0.onrender.com",
 })
 API.interceptors.request.use((req)=>{
     const token=localStorage.getItem("token");
